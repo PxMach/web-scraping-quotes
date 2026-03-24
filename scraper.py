43832191e108e4e9420ddae8c66621731946f52a
PxMach <moundjidmachghour58@gmail.com>
2026-03-24 10:04:29 +0100

    Refactor scraper to write quotes and authors to CSV file

--- scraper.py
+++ scraper.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page_to_scrape = requests.get("https://quotes.toscrape.com/")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-quotes  = soup.find_all("span", attrs={"class":"text"})
-authors = soup.find_all("small", attrs={"class":"author"})
-tags = soup.find_all("a", class_="tag")
+quotes = soup.find_all("span", attrs={"class": "text"})
+authors = soup.find_all("small", attrs={"class": "author"})
 
-for quote, author, tag in zip(quotes, authors, tags):
-    print(quote.text + " . " + author.text + tag.text) 
+
+file = open("scraped_quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+
+for quote, author in zip(quotes, authors):
+    print(quote.text + " . " + author.text)
+    writer.writerow([quote.text, author.text])
+file.close()
